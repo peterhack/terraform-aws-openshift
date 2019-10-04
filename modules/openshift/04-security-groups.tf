942925fc04cc6e7a64cb0e82820d8384c4a7ce13
Peter Hack <peter.hack@dynatrace.com>
2019-10-04 15:01:35 -0400

    adding egress for 8443

--- modules/openshift/04-security-groups.tf
+++ modules/openshift/04-security-groups.tf
@@ -99,6 +99,14 @@ resource "aws_security_group" "openshift-public-egress" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  //  ALT
+  egress {
+    from_port   = 8443
+    to_port     = 8443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   //  Use our common tags and add a specific name.
   tags = "${merge(
     local.common_tags,
